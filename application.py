c06bf760e6ac993d26812ef1ea5c3acb947b3c34
Andrew Symes <andrew.symes1@ucalgary.ca>
2022-02-06 21:51:13 -0700

    getting JSON Going

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 import os
 import re
+import requests
+import json
 
 
 from flask import Flask, session
@@ -74,3 +76,20 @@ def search():
     password = request.form.get("password")
 
     return render_template("search.html",username=username, password = password)
+
+@app.route("/api/<str:ISBN>")
+def api():
+    res = requests.get("https://www.googleapis.com/books/v1/volumes", params={"q": "isbn:{{ISBN}}"})
+    bookinfo = res.json()
+    bookinfo = (bookinfo["items"][0])
+    title = (bookinfo["volumeInfo"]["title"])
+    authors = (bookinfo["volumeInfo"]["authors"])
+    publishedDate = (bookinfo["volumeInfo"]["publishedDate"])
+    ISBN_10 = (bookinfo["volumeInfo"]["industryIdentifiers"][0]["identifier"])
+    ISBN_13 = (bookinfo["volumeInfo"]["industryIdentifiers"][1]["identifier"])
+    reviewCount = (bookinfo["volumeInfo"]["ratingsCount"])
+    averageRating = (bookinfo["volumeInfo"]["averageRating"])
+
+    infor = {"title": title, "authors": authors, "publishedDate": publishedDate, "ISBN_10": ISBN_10, "ISBN_13": ISBN_13,"reviewCount": reviewCount,"averageRating": averageRating}
+
+    return(json.dumps(infor))
--- testingGOOG.py
+++ testingGOOG.py
@@ -1,3 +1,21 @@
 import requests
+import json
+
 res = requests.get("https://www.googleapis.com/books/v1/volumes", params={"q": "isbn:080213825X"})
-print(res.json())
+bookinfo = res.json()
+bookinfo = (bookinfo["items"][0])
+title = (bookinfo["volumeInfo"]["title"])
+authors = (bookinfo["volumeInfo"]["authors"])
+publishedDate = (bookinfo["volumeInfo"]["publishedDate"])
+ISBN_10 = (bookinfo["volumeInfo"]["industryIdentifiers"][0]["identifier"])
+ISBN_13 = (bookinfo["volumeInfo"]["industryIdentifiers"][1]["identifier"])
+reviewCount = (bookinfo["volumeInfo"]["ratingsCount"])
+averageRating = (bookinfo["volumeInfo"]["averageRating"])
+
+print(averageRating)
+
+
+
+infor = {"title": title, "authors": authors, "publishedDate": publishedDate, "ISBN_10": ISBN_10, "ISBN_13": ISBN_13,"reviewCount": reviewCount,"averageRating": averageRating}
+
+print(json.dumps(infor))
